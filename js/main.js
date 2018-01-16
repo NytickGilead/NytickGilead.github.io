6356dab07bbe5205b72f862cbff972f27f715c2c
Dmitry <35427139+NytickGilead@users.noreply.github.com>
2018-01-16 22:46:02 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,50 @@
+		$(document).ready(function (){
+			$(".services-item1 button").click(function(){
+				$('.services-item1').hide("slow");
+				$('#services-item1').show('slow');
+				$("button#times1").click(function(){
+					$('.services-item1').css({'display' : 'block'});
+					$('#services-item1').css({'display' : 'none'});
+				});
+			});
+			$(".services-item2 button").click(function(){
+				$('.services-item2').hide("slow");
+				$('#services-item2').show('slow');
+				$("button#times2").click(function(){
+					$('.services-item2').css({'display' : 'block'});
+					$('#services-item2').css({'display' : 'none'});
+				});
+			});
+			$(".services-item3 button").click(function(){
+				$('.services-item3').hide("slow");
+				$('#services-item3').show('slow');
+				$("button#times3").click(function(){
+					$('.services-item3').css({'display' : 'block'});
+					$('#services-item3').css({'display' : 'none'});
+				});
+			});
+			$(".services-item4 button").click(function(){
+				$('.services-item4').hide("slow");
+				$('#services-item4').show('slow');
+				$("button#times4").click(function(){
+					$('.services-item4').css({'display' : 'block'});
+					$('#services-item4').css({'display' : 'none'});
+				});
+			});
+			$(".services-item5 button").click(function(){
+				$('.services-item5').hide("slow");
+				$('#services-item5').show('slow');
+				$("button#times5").click(function(){
+					$('.services-item5').css({'display' : 'block'});
+					$('#services-item5').css({'display' : 'none'});
+				});
+			});
+			$(".services-item6 button").click(function(){
+				$('.services-item6').hide("slow");
+				$('#services-item6').show('slow');
+				$("button#times6").click(function(){
+					$('.services-item6').css({'display' : 'block'});
+					$('#services-item6').css({'display' : 'none'});
+				});
+			});
+		});
